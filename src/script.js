dd62c08647eb0f4f7916cf914b1631ec8fc3d6e0
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-07-08 14:38:32 +0200

    react setup done

---
+++ src/script.js
@@ -0,0 +1,10 @@
+export const script = () => {
+    return (
+        <div>
+            <h1>Hello wolrd !</h1>
+            <h1>Hello wolrd !</h1>
+            <h1>Hello wolrd !</h1>
+            <h1>Hello wolrd !</h1>
+        </div>
+    );
+};
