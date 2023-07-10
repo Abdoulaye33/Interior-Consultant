275d5d02c961ae308daa095ba7c444a0716d00af
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-07-10 09:48:06 +0200

    desktop html done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { script as ScriptComponent } from './script';
+import { Desktop } from './script';
 
 function App() {
   return (
     <div>
       
-      <ScriptComponent />
+      <Desktop />
 
     </div>
     
---
+++ src/script.jsx
@@ -0,0 +1,32 @@
+export const Desktop = () => {
+    return (
+        <div>
+            <header>
+                <ul>
+                    <li>This Interior</li>
+                    <li>Home</li>
+                    <li>Collection</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </header>
+            <main>
+                <div>
+                    <h1>Modern interior</h1>
+                    <p>A full-service residential & commercial interior design and staging company offering professional organizing & eco-services.</p>
+                    <button>Read more </button>
+                </div>
+                <div>
+                    <div>
+                        <img src="../src/img/photo2.png" alt="" />
+                        <ul>
+                            <li><h3>Aliza webber</h3></li>
+                            <li>interior designer</li>
+                        </ul>
+                        <h2>Designed in 2020 by Aliza Webber</h2>
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+};
